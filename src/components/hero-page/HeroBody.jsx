8fa5a975a2cc0116ab5aa831943a692375b34e77
Patricio RT <patriosweb@gmail.com>
2022-01-21 19:58:29 +0000

    bug fixed

--- src/components/hero-page/HeroBody.jsx
+++ src/components/hero-page/HeroBody.jsx
@@ -6,38 +6,49 @@ import User from "./nav-tools/User";
 import CartIndex from "./nav-tools/cart/CartIndex";
 
 const HeroBody = ({ cartItems, onAdd, onRemove, access, setAccess }) => {
-	return (
-		<div className="HeroBody">
-			<div className="hero-body">
-				<Switch>
-					<Route exact path="/signup">
-						<SignUp />
-					</Route>
-					<Route exact path="/login">
-						<LogIn access={access} setAccess={setAccess} />
-					</Route>
-					<Route exact path="/user">
-						<User />
-					</Route>
-					<Route exact path="/cart">
-						<CartIndex cartItems={cartItems} onAdd={onAdd} onRemove={onRemove} />
-					</Route>
-					<Route exact path="/">
-						<div className="hero-content">
-							<div className="hero-text">
-								<h1>Las pizzas más cremas de Gran Canaria!</h1>
-								<div>
-									<button className="hero-btn" onClick={() => window.scrollTo(0, 625)}>
-										Haz tu pedido aquí!
-									</button>
-								</div>
-							</div>
-						</div>
-					</Route>
-				</Switch>
-			</div>
-		</div>
-	);
+  return (
+    <div className="HeroBody">
+      <div className="hero-body">
+        <Switch>
+          <Route exact path="/signup">
+            <SignUp />
+          </Route>
+          <Route exact path="/login">
+            <LogIn access={access} setAccess={setAccess} />
+          </Route>
+          <Route exact path="/user">
+            <User />
+          </Route>
+          <Route exact path="/cart">
+            <CartIndex
+              cartItems={cartItems}
+              onAdd={onAdd}
+              onRemove={onRemove}
+            />
+          </Route>
+          <Route exact path="/">
+            <div className="hero-content">
+              <div className="hero-text">
+                <h1>Las pizzas más cremas de Gran Canaria!</h1>
+                <div>
+                  <button
+                    className="hero-btn"
+                    onClick={() =>
+                      document
+                        .querySelector("section")
+                        .scrollIntoView({ behavior: "smooth" })
+                    }
+                  >
+                    Haz tu pedido aquí!
+                  </button>
+                </div>
+              </div>
+            </div>
+          </Route>
+        </Switch>
+      </div>
+    </div>
+  );
 };
 
 export default HeroBody;
--- src/components/products/index.js
+++ src/components/products/index.js
@@ -1,18 +1,26 @@
-import { ProductsContainer, ProductHeading, ProductWrapper } from "./ProductsElements";
+import {
+  ProductsContainer,
+  ProductHeading,
+  ProductWrapper,
+} from "./ProductsElements";
 import SingleProduct from "./SingleProduct";
 
 const Products = ({ heading, pizzaList, onAdd }) => {
-	console.log("pizzaList", pizzaList);
-	return (
-		<ProductsContainer>
-			<ProductHeading>{heading}</ProductHeading>
-			<ProductWrapper>
-				{pizzaList.map((product, index) => {
-					return <SingleProduct product={product} key={index} onAdd={onAdd} />;
-				})}
-			</ProductWrapper>
-		</ProductsContainer>
-	);
+  console.log("pizzaList", pizzaList);
+  return (
+    <section>
+      <ProductsContainer>
+        <ProductHeading>{heading}</ProductHeading>
+        <ProductWrapper>
+          {pizzaList.map((product, index) => {
+            return (
+              <SingleProduct product={product} key={index} onAdd={onAdd} />
+            );
+          })}
+        </ProductWrapper>
+      </ProductsContainer>
+    </section>
+  );
 };
 
 export default Products;
